9476da9df37f80f652446d69af83d51d3b591f1a
Stywar <franco111988@gmail.com>
2020-12-04 21:24:36 -0500

    se agrego files

---
+++ src/main/java/org/aforo255/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package org.aforo255.repository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.aforo255.entity.Transaction;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+@ApplicationScoped
+public class TransactionRepository implements PanacheRepositoryBase<Transaction, Integer> {
+
+}
